54fb95ca29409d40febe05123a870752db1db351
Kunalhume <78203037+Kunalhume@users.noreply.github.com>
2023-02-13 01:19:04 +0530

    Kunal

---
+++ index.js
@@ -0,0 +1,51 @@
+// const app = require('./app')
+// // console.log(app);
+// // console.log(app.x);
+// // console.log(app.y);
+// const arr=[2,4,6,8,10];
+// // console.log(arr[4]);
+
+// let rv =arr.filter((item)=>{
+//   // console.log(item)
+//   return item>=3;
+// })
+// console.log(rv);
+
+// const fs = require('fs');
+// fs.writeFileSync("HelloKunal1.txt","Kunal your on right way Great way -----> *You Can Do It*")
+
+// console.log(">>>",__dirname,"<<<");
+// console.log(">>>",__filename,"<<<");
+
+// const ks = require('fs');
+// ks.writeFileSync("kodec.txt","Welcome Kodec in Node JS Learning ");
+
+// Optimize way
+// const king = require('fs').writeFileSync;
+// king("Hey.txt","Hey, Buddy");
+
+// ------------> Lecture -7 <---------------
+
+// const http = require("http");
+
+// const dataControl =(req, resp)=> {
+//   resp.write("<h1>Hello Kunal, Well Done! Buddy</h1>");
+//   resp.end();
+// }
+
+// http.createServer(dataControl).listen(4500);
+
+// http.createServer((req,resp)=> {
+//   resp.write("<h1>Hello Kunal, Well Done! Buddy Nice @Kunal</h1>");
+//   resp.end();
+// }).listen(4500);
+
+
+// -------------> Lecture - 8 <------------------
+// Learning package.json File in Node JS
+
+// const colors= require('colors');
+// console.log("package.json".green);
+
+
+// --------------> Lecture - 9  <-------------------
